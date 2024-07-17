1fbe67d04f0a25ea49f98db8a98feb07e78bcc21
jia-wei-00 <wwwbingo6@gmail.com>
2024-07-18 01:19:40 +0800

    update code to sleep

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,34 @@
 import {
   DarkTheme,
   DefaultTheme,
-  NavigationContainer,
   ThemeProvider,
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { NativeBaseProvider, useColorMode, useTheme } from "native-base";
+import { NativeBaseProvider, extendTheme, Text } from "native-base";
 import React from "react";
-import { Appearance, Platform, useColorScheme } from "react-native";
+import { Appearance } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// Define the config
+const config = {
+  useSystemColorMode: true,
+  initialColorMode: "dark",
+};
+
+const customTheme = extendTheme({
+  config,
+});
 
 export default function RootLayout() {
   return (
-    <NativeBaseProvider>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
-    </NativeBaseProvider>
+    <ThemeProvider
+      value={Appearance.getColorScheme() === "dark" ? DarkTheme : DefaultTheme}
+    >
+      <NativeBaseProvider theme={customTheme}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </NativeBaseProvider>
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -8,22 +8,35 @@ import {
   Box,
   Container,
 } from "native-base";
+import { Appearance } from "react-native";
 
 export default function Index() {
   const { colorMode, toggleColorMode } = useColorMode();
   const text = useColorModeValue("Light", "Dark");
   const bg = useColorModeValue("warmGray.50", "coolGray.800");
 
+  const handleButton = () => {
+    toggleColorMode();
+    Appearance.setColorScheme(colorMode);
+  };
+
   return (
     <Center>
-      <Box>
+      <Box
+        _dark={{
+          bg: "coolGray.800",
+        }}
+        _light={{
+          bg: "warmGray.50",
+        }}
+      >
         <Text fontSize="lg" display="flex" mb={20}>
           The active color mode is {""}
           <Text bold fontSize="18px">
             {text}
           </Text>
         </Text>
-        <Button onPress={toggleColorMode} h={10}>
+        <Button onPress={handleButton} h={10}>
           Toggle
         </Button>
       </Box>
