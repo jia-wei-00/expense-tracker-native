1a3eb5df11ac504137d73981765ed24db89ebb70
jia-wei-00 <wwwbingo6@gmail.com>
2025-06-09 01:02:05 +0800

    chore: update

--- store/features/database-store/loan-record-slice/add-loan-record.ts
+++ store/features/database-store/loan-record-slice/add-loan-record.ts
@@ -1,7 +1,22 @@
 import { supabase } from "@/supabase";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { LoanRecord } from "./loan-record-slice";
 
 export const addLoanRecord = createAsyncThunk(
   "loanRecord/addLoanRecord",
-  async (loanRecord: LoanRecord) => {}
+  async (loanRecord: LoanRecord) => {
+    try {
+      const { data, error } = await supabase
+        .from("loan_record")
+        .insert(loanRecord);
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      return data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
 );
