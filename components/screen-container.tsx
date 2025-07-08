3380c9c2fd2ee829abad985510a54c04ca0750f3
jia-deriv <jiawei@regentmarkets.com>
2025-07-08 20:32:22 +0800

    update tab

--- components/screen-container.tsx
+++ components/screen-container.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { VStack } from "@/components/ui/vstack";
-import { ScrollView } from "react-native";
+import { ScrollView, useWindowDimensions } from "react-native";
 import { twMerge } from "tailwind-merge";
 import {
   OnTabChangeCallback,
   TabBarProps,
   Tabs,
 } from "react-native-collapsible-tab-view";
-import { TabView, SceneMap } from "react-native-tab-view";
+import { TabView, SceneMap, SceneRendererProps } from "react-native-tab-view";
 // import { ScrollView } from "react-native-gesture-handler";
 
 type TabScreen = {
@@ -21,9 +21,10 @@ interface ScreenContainerProps extends React.ComponentProps<typeof ScrollView> {
   vStackClassName?: string;
   stickyContentClassName?: string;
   stickyContent?: React.ReactNode;
-  index?: number;
+  initialIndex?: number;
   tabScreens?: TabScreen[];
-  tabBar?: (props: TabBarProps<string>) => React.ReactElement;
+  // tabBar?: (props: TabBarProps<string>) => React.ReactElement;
+  tabBar?: (props: SceneRendererProps) => React.ReactNode;
   onTabChange?: OnTabChangeCallback<string>;
 }
 
@@ -33,15 +34,15 @@ const ScreenContainer = ({
   className,
   stickyContent,
   stickyContentClassName,
-  index = 0,
+  initialIndex = 0,
   tabScreens,
   refreshControl,
   tabBar,
   onTabChange,
   ...rest
 }: ScreenContainerProps) => {
-  const ref = React.useRef(null);
   const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(initialIndex);
 
   const routes = tabScreens?.map((item) => ({
     key: item.key,
@@ -84,11 +85,12 @@ const ScreenContainer = ({
 
   return (
     <>
-      {tabScreens && tabScreens.length > 0 ? (
+      {tabScreens ? (
         <VStack
           className={twMerge("bg-background-0 flex-1", className)}
           {...rest}
         >
+          {stickyContent && renderHeader()}
           {/* <Tabs.Container
             renderHeader={renderHeader}
             revealHeaderOnScroll
@@ -107,12 +109,15 @@ const ScreenContainer = ({
               </Tabs.Tab>
             ))}
           </Tabs.Container> */}
-          <TabView
-            navigationState={{ index, routes }}
-            renderScene={renderScene}
-            onIndexChange={setIndex}
-            initialLayout={{ width: layout.width }}
-          />
+          {routes && renderScene && tabBar && (
+            <TabView
+              navigationState={{ index, routes }}
+              renderScene={renderScene}
+              onIndexChange={setIndex}
+              initialLayout={{ width: layout.width }}
+              renderTabBar={tabBar}
+            />
+          )}
         </VStack>
       ) : (
         <ScrollView
@@ -136,155 +141,3 @@ const ScreenContainer = ({
 };
 
 export default ScreenContainer;
-
-import React from "react";
-import { VStack } from "@/components/ui/vstack";
-import { ScrollView, useWindowDimensions } from "react-native";
-import { twMerge } from "tailwind-merge";
-import { View, StyleSheet, ListRenderItem } from "react-native";
-import { Tabs } from "react-native-collapsible-tab-view";
-import { TabView, SceneMap } from "react-native-tab-view";
-
-const HEADER_HEIGHT = 250;
-
-const DATA = [0, 1, 2, 3, 4];
-const identity = (v: unknown): string => v + "";
-
-const Header = () => {
-  return <View style={styles.header} />;
-};
-
-type SceneMapping = {
-  key: string;
-  render: React.ReactNode;
-  title: string;
-};
-
-interface ScreenContainerProps extends React.ComponentProps<typeof ScrollView> {
-  children: React.ReactNode;
-  vStackClassName?: string;
-  stickyContentClassName?: string;
-  stickyContent?: React.ReactNode;
-  index?: number;
-  sceneMapping: SceneMapping[];
-  setIndex: (index: number) => void;
-}
-
-const ScreenContainer = ({
-  children,
-  vStackClassName,
-  className,
-  stickyContent,
-  stickyContentClassName,
-  index = 0,
-  sceneMapping,
-  setIndex,
-  ...rest
-}: ScreenContainerProps) => {
-  const layout = useWindowDimensions();
-
-  const renderScene = SceneMap(
-    Object.fromEntries(
-      sceneMapping.map((item) => [
-        item.key,
-        () => (
-          <ScrollView>
-            <VStack
-              space="md"
-              className={twMerge(vStackClassName, "bg-background-0 pb-4")}
-            >
-              {item.render}
-            </VStack>
-          </ScrollView>
-        ),
-      ])
-    )
-  );
-
-  const routes = sceneMapping.map((item) => ({
-    key: item.key,
-    title: item.title,
-  }));
-
-  const renderHeader = () => {
-    return (
-      <VStack
-        space="md"
-        className={twMerge(
-          stickyContentClassName,
-          "bg-background-0 py-4 shadow-sm"
-        )}
-      >
-        {stickyContent}
-      </VStack>
-    );
-  };
-
-  return (
-    <>
-      <VStack
-        className={twMerge("bg-background-0 flex-1", className)}
-        // stickyHeaderIndices={[0]}
-        // scrollEventThrottle={16}
-        // showsVerticalScrollIndicator={false}
-        {...rest}
-      >
-        <VStack
-          space="md"
-          className={twMerge(
-            stickyContentClassName,
-            "bg-background-0 py-4 shadow-sm"
-          )}
-        >
-          {stickyContent}
-        </VStack>
-        <TabView
-          navigationState={{ index, routes }}
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          initialLayout={{ width: layout.width }}
-        />
-
-        {/* <VStack
-          space="md"
-          className={twMerge(vStackClassName, "bg-background-0 pb-4")}
-        >
-          {children}
-        </VStack> */}
-      </VStack>
-      {/* <Tabs.Container
-        renderHeader={renderHeader}
-        revealHeaderOnScroll
-        headerContainerStyle={{
-          backgroundColor: "--color-background-0",
-        }}
-      >
-        <Tabs.Tab name="A">
-          <Tabs.ScrollView>{children}</Tabs.ScrollView>
-        </Tabs.Tab>
-        <Tabs.Tab name="B">
-          <Tabs.ScrollView>{children}</Tabs.ScrollView>
-        </Tabs.Tab>
-      </Tabs.Container> */}
-    </>
-  );
-};
-
-export default ScreenContainer;
-const styles = StyleSheet.create({
-  box: {
-    height: 250,
-    width: "100%",
-  },
-  boxA: {
-    backgroundColor: "white",
-  },
-  boxB: {
-    backgroundColor: "#D8D8D8",
-  },
-  header: {
-    height: HEADER_HEIGHT,
-    width: "100%",
-    backgroundColor: "#2196f3",
-  },
-});
